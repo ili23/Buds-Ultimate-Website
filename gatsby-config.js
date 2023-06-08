b113dbf8e2bc5f3bdfc772dbdcefa0e8aa432252
Iram Liu <iramliu@Irams-MacBook-Air.local>
2023-06-07 21:15:14 -0500

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Buds Ultimate Frisbee`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-postcss"]
+};
